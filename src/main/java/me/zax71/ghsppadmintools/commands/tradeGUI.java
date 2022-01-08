039d89d8466605d5e7b280303829568fbd5261a1
Zax71 <zabax32@gmail.com>
2022-01-08 17:59:03 +0000

    Add tradeGUI.java

---
+++ src/main/java/me/zax71/ghsppadmintools/commands/tradeGUI.java
@@ -0,0 +1,15 @@
+package me.zax71.ghsppadmintools.commands;
+
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.annotation.CommandPermission;
+import co.aikar.commands.annotation.Default;
+import org.bukkit.command.CommandSender;
+
+public class tradeGUI extends BaseCommand {
+
+    @Default
+    @CommandPermission("GHSPP.material")
+    public static void onTradeGUI(CommandSender sender, String[] args) {
+
+    }
+}
