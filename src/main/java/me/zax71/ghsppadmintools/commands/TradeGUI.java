c82310e24717d9c7eeb08ad5f2b99ed97710cebf
Zax71 <zabax32@gmail.com>
2022-01-10 18:32:13 +0000

    Fix arg[0] player usage

--- src/main/java/me/zax71/ghsppadmintools/commands/TradeGUI.java
+++ src/main/java/me/zax71/ghsppadmintools/commands/TradeGUI.java
@@ -2,6 +2,7 @@ package me.zax71.ghsppadmintools.commands;
 
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.CommandCompletion;
 import co.aikar.commands.annotation.CommandPermission;
 import co.aikar.commands.annotation.Default;
 import me.zax71.ghsppadmintools.GHSPPAdminTools;
@@ -16,21 +17,35 @@ public class TradeGUI extends BaseCommand {
 
     @Default
     @CommandPermission("GHSPP.tradeGUI")
+    @CommandCompletion("@players")
     public static void onTradeGUI(Player sender, String[] args) {
-        if (args.length == 1) {
-            sender = Bukkit.getServer().getPlayer(args[0]);
+
+        // Define empty player object
+        Player player = null;
+
+        // If there are no arguments...
+        if (args.length == 0) {
+            // ...set senderPlayer to the sender
+            player = sender;
+            // if there are arguments
+        } else {
+            // if the player is online
+            if (Bukkit.getOnlinePlayers().toString().toLowerCase().contains(args[0].toLowerCase())) {
+                player = Bukkit.getPlayer(args[0]);
+            } else {
+                GHSPPAdminTools.plugin.getLogger().warning("/tradeGUI must be used with online players");
+            }
         }
-        Player finalSender = sender;
 
         new AnvilGUI.Builder()
-                .onComplete((player, text) -> {
-                    Bukkit.dispatchCommand(finalSender, "trade " + text);
+                .onComplete((completePlayer, text) -> {
+                    Bukkit.dispatchCommand(completePlayer, "trade " + text);
                     return AnvilGUI.Response.close();
                 })
                 .text("Player Name")
                 .itemLeft(new ItemStack(Material.PAPER))
                 .title("Player to trade with")
                 .plugin(GHSPPAdminTools.plugin)
-                .open(sender);
+                .open(player);
     }
 }
