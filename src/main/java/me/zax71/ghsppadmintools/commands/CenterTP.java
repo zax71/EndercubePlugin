21d1a681240c16cd00b9fff8da49d7f0962b013e
Zax71 <zabax32@gmail.com>
2022-01-16 10:17:50 +0000

    Add CenterTP.java

---
+++ src/main/java/me/zax71/ghsppadmintools/commands/CenterTP.java
@@ -0,0 +1,10 @@
+package me.zax71.ghsppadmintools.commands;
+
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.annotation.CommandAlias;
+
+@CommandAlias("CenterTP|center|TPMid")
+public class CenterTP extends BaseCommand {
+
+
+}
