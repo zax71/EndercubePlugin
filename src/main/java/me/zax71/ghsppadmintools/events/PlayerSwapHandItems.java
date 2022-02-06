888e17360f72cb0879bbc13d99372352e23a021c
Zax71 <zabax32@gmail.com>
2022-02-06 16:03:53 +0000

    Add config option to disable Fcommand bump version to 1.5.1

--- src/main/java/me/zax71/ghsppadmintools/events/PlayerSwapHandItems.java
+++ src/main/java/me/zax71/ghsppadmintools/events/PlayerSwapHandItems.java
@@ -14,33 +14,35 @@ public class PlayerSwapHandItems implements Listener {
 
     @EventHandler
     public void onPlayerSwapHandItemsEvent(PlayerSwapHandItemsEvent e) {
+        if (GHSPPAdminTools.plugin.getConfig().getBoolean("Fcommand.enabled")) {
+            // Get the player
+            Player p = e.getPlayer();
 
-        // Get the player
-        Player p = e.getPlayer();
+            // Cancel the event
+            e.setCancelled(true);
 
-        // Cancel the event
-        e.setCancelled(true);
-
-        // Get the values from config
-        List<?> commands = GHSPPAdminTools.plugin.getConfig().getList("commands");
+            // Get the values from config
+            List<?> commands = GHSPPAdminTools.plugin.getConfig().getList("Fcommand.commands");
 
 
 
-        if (commands != null) {
-            // Iterate through the list
-            for (int i = 0; i < commands.size(); i++) {
+            if (commands != null) {
+                // Iterate through the list
+                for (int i = 0; i < commands.size(); i++) {
 
-                // Get the current working command as a variable
-                String currentCommand = String.valueOf(commands.get(i));
+                    // Get the current working command as a variable
+                    String currentCommand = String.valueOf(commands.get(i));
 
-                // Phrase it with PAPI
-                String papiCommand = PlaceholderAPI.setPlaceholders(p, currentCommand);
+                    // Phrase it with PAPI
+                    String papiCommand = PlaceholderAPI.setPlaceholders(p, currentCommand);
 
-                // Send the commands
-                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), papiCommand);
+                    // Send the commands
+                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), papiCommand);
+                }
+            } else {
+                GHSPPAdminTools.plugin.getLogger().severe("No commands are in config.yml, add at least one!");
             }
-        } else {
-            GHSPPAdminTools.plugin.getLogger().severe("No commands are in config.yml, add at least one!");
         }
+
     }
 }
