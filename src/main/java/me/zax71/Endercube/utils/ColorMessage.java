5d30b170d8de8a4768513a6725e4a87f3d36e360
Zax71 <zabax32@gmail.com>
2022-03-12 10:27:45 +0000

    oops, forgot the .0 on the version...

--- src/main/java/me/zax71/Endercube/utils/ColorMessage.java
+++ src/main/java/me/zax71/Endercube/utils/ColorMessage.java
@@ -3,7 +3,7 @@ package me.zax71.Endercube.utils;
 import org.bukkit.ChatColor;
 
 public class ColorMessage {
-    public String color(String message) {
+    public static String color(String message) {
         return ChatColor.translateAlternateColorCodes('&', message);
     }
 }
