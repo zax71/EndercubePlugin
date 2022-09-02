711de2f48c414f1e22172ac8fba45ce5a0d910c8
Zax71 <zabax32@gmail.com>
2022-09-02 19:09:20 +0100

    Stop join events from executing when it is a bedrock player

--- src/main/java/me/zax71/Endercube/events/PlayerJoin.java
+++ src/main/java/me/zax71/Endercube/events/PlayerJoin.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.geysermc.floodgate.api.FloodgateApi;
 
 import java.util.Set;
 import java.util.UUID;
@@ -17,22 +18,33 @@ public class PlayerJoin implements Listener {
 
     @EventHandler
     public void onPlayerJoinEvent(PlayerJoinEvent e) {
-        // Get config values
 
+        // Get config values
         Boolean joinEnabled = plugin.getConfig().getBoolean("joinMessage.enabled");
         Boolean resourcePack = plugin.getConfig().getBoolean("joinMessage.resourcePack");
 
         Player player = e.getPlayer();
+        UUID playerUUID = player.getUniqueId();
 
         if (joinEnabled.equals(Boolean.TRUE)) {
-            if (resourcePack.equals(Boolean.FALSE)) {
 
-                e.joinMessage(null);
-                ActionExecuter.execute("joinMessage.actions", player);
+            // Set the vanilla join message to null, letting us use our own
+            e.joinMessage(null);
+
+            // Does the user want to wait for the RP to load
+            if (resourcePack.equals(Boolean.TRUE)) {
+
+                // Are they a bedrock user?
+                if (FloodgateApi.getInstance().isFloodgatePlayer(playerUUID)) {
+                    ActionExecuter.execute("joinMessage.actions", player);
+                } else {
+                    // Add the players UUID to our hashmap, so we can read it later when the pack loads
+                    Set<UUID> messageSent = Endercube.ResourcePackLoginMessageSent;
+                    messageSent.add(playerUUID);
+                }
+            // No waiting for the RP
             } else {
-                e.joinMessage(null);
-                Set<UUID> messageSent = Endercube.ResourcePackLoginMessageSent;
-                messageSent.add(player.getUniqueId());
+                ActionExecuter.execute("joinMessage.actions", player);
             }
         }
     }
